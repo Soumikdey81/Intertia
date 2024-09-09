bfcd1b154753f0d08c6d861f814bf9336c4ed717
Soumikdey81 <aditdey30@gmail.com>
2024-09-09 22:02:56 +0530

    Soumikdey81

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('app', [
+        "name"=> "soumik Dey",
+        "frameworks" => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
+    ]);
 });
