ada074cdc5b0be21ae367ddc5b567dc443b8d48f
Soumikdey81 <aditdey30@gmail.com>
2024-09-10 17:33:24 +0530

    Soumikdey81

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+    <p>In this Series, we will use this followng frameworks:</p>
+
+    <ul>
+        <li v-for="framework of frameworks" :key="framework">
+            {{ framework }}
+        </li>
+    </ul>
+
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/users">Users</a></li>
+            <li><a href="/settings">Settings</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    },
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,18 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('app', [
+    return Inertia::render('home', [
         "name"=> "soumik Dey",
         "frameworks" => [
             'Laravel', 'Vue', 'Inertia'
         ]
     ]);
+});
+
+Route::get('/users', function(){
+    return Inertia::render('users');
+});
+
+Route::get('/settings', function(){
+    return Inertia::render('settings');
 });
